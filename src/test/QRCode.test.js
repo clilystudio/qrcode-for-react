7501840428dd6120c6b7a064281a607f4063c959
bullsgl <bullsgl@gmail.com>
2022-11-18 14:16:47 +0800

    Jest

--- src/test/QRCode.test.js
+++ src/test/QRCode.test.js
@@ -3,5 +3,6 @@ import QRCode from "../lib/components/core/QRCode";
 test('QRCode Test 1', () => {
   let data = 'ABC123';
   let config = {};
-  QRCode.generate(data,config);
+  const matrix = QRCode.generate(data,config);
+  console.log(matrix);
 });
